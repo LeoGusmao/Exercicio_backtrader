1b6e9803f22ad629bb0f20eef94f9cd21a40000c
LeoGusmao <leonardogusmaopsi@yahoo.com.br>
2024-01-08 07:58:44 -0300

    versão original baixada

---
+++ basic_setup.py
@@ -0,0 +1,24 @@
+import backtrader as bt
+
+# inicializa nossa engine de backtest
+cerebro = bt.Cerebro()
+
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+cerebro.broker.setcash(2500)
+
+# executa nosso backtest
+cerebro.run()
+
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+# vamos configurar o valor inicial para ser usado no backtest
+
+cerebro = bt.Cerebro()
+cerebro.broker.setcash(50000.0)
+
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+cerebro.run()
+
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
